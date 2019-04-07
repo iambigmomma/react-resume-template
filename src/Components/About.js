b4b5f73292520bce52fe431dda8fb66c46fd6816
iambigmomma <iambigmomma@gmail.com>
2019-04-07 23:49:43 +0200

    Revised personal information and remove testminal

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,14 +20,14 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Jeff Fan Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
             <p>{bio}</p>
             <div className="row">
-               <div className="columns contact-details">
+               {/* <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
@@ -37,7 +37,7 @@ class About extends Component {
 						   <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
-               </div>
+               </div> */}
                <div className="columns download">
                   <p>
                      <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
